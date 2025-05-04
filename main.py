13bd4052735feb350139d63404569212e356ddf4
shmuel keller <shmuli@Mac.attlocal.net>
2025-05-04 18:23:10 -0400

    ...

--- main.py
+++ main.py
@@ -1,7 +1,68 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+import sqlite3
+import os
+from routers.auth import auth_router
+from routers.google_sheets.router import sheets_router
+from expense import ExpenseResponse
+
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+port = os.getenv("PORT", 9090)
+print(f'{port = }')
+
+
+
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+app.mount("/assets", StaticFiles(directory="frontend/dist/assets"), name="assets")
+templates = Jinja2Templates("frontend/dist")
+
+
+
+app.include_router(auth_router, prefix="/auth")
+app.include_router(sheets_router, prefix="/expenses")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+from init_db_ import init_db
+
+init_db()
+
+from auth_baseModels import UserCreate, UserLogin
+
+
+
+@app.get("/expenses", response_model=List[ExpenseResponse])
+def get_expenses():
+    return []
+
+
+@app.get("/", response_model=List[ExpenseResponse])
+def get_expenses(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/categories")
+def get_categories():
+    return [
+        "Food", "Gifts", "Health/medical", "Home", "Transportation", 
+        "Personal", "Pets", "Utilities", "Travel", "Debt", "Other"
+    ]
+
+
+
---
+++ routers/google_sheets/talk_to_sheets.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+from expense import Expense
+from utils.date import get_current_month
+
+
+
+
+credentials_file = "credentials.json"
+
+
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    # "https://www.googleapis.com/auth/drive",
+]
+
+credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
+client = gspread.Client(credentials)
+
+
+
+def get_first_empty_transaction_row(worksheet):
+    try:
+        server_data_section: str = worksheet.cell("2D").value
+        server_data = json.loads(server_data_section)
+        transactions_row_upto = server_data["next empty transaction row"]
+    except:
+        transactions_start_row = 5
+        transactions_row_upto = transactions_start_row
+
+
+    empty_found = False
+    while not empty_found:
+        row_values = worksheet.row_values(transactions_row_upto)
+        if row_values == []:
+            empty_found = True
+            break
+        
+        transactions_row_upto += 1
+    return transactions_row_upto
+
+
+
+
+
+
+
+
+
+
+categories = [
+    "Food",
+    "Gifts",
+    "Health/medical",
+    "Home",
+    "Transportation",
+    "Personal",
+    "Pets",
+    "Utilities",
+    "Travel",
+    "Debt",
+    "Other"
+]
+
+
+
+
+
+month_map = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+
+def create_row_in_sheets(expense: Expense, sheet_id):
+    sheet = client.open_by_key(sheet_id)
+    month_as_string: str = month_map[get_current_month()]
+    worksheet = sheet.worksheet(month_as_string)
+    first_empty_transaction_row = get_first_empty_transaction_row(worksheet)
+    worksheet.update(f"B{first_empty_transaction_row}:E{first_empty_transaction_row}", [[expense.date, expense.amount, expense.description, expense.category]])
+    SERVER_DATA_SECTION = json.dumps({"next empty transaction row": first_empty_transaction_row + 1 })
+    worksheet.update_cell(2, 4, SERVER_DATA_SECTION)
+
+
+
